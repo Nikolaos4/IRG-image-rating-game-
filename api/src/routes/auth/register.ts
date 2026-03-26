dd6426c44794438655e658b10e05f322ca521e13
nekotyan2d <dmitryrychazhkov2019@gmail.com>
2026-03-26 13:27:28 +0300

    fix: fix register behaviour when user enters existing email

--- api/src/routes/auth/register.ts
+++ api/src/routes/auth/register.ts
@@ -3,6 +3,7 @@ import { z } from "zod";
 import { FastifyZodOpenApiTypeProvider } from "fastify-zod-openapi";
 import bcrypt from "bcrypt";
 import { prisma } from "@/lib/prisma.js";
+import { PrismaClientKnownRequestError } from "@/generated/prisma/internal/prismaNamespace.js";
 
 export const RegisterRequestBody = z.object({
     email: z.email().meta({ example: "you@example.com" }),
@@ -25,26 +26,39 @@ export default async function register(app: FastifyInstance) {
             const { email, username, password } = request.body as z.infer<typeof RegisterRequestBody>;
 
             const hashedPassword = await bcrypt.hash(password, 10);
-            const user = await prisma.user.create({
-                data: {
-                    email,
-                    username,
-                    password: hashedPassword,
-                    role: {
-                        connect: { role_id: 1 },
+
+            try {
+                const user = await prisma.user.create({
+                    data: {
+                        email,
+                        username,
+                        password: hashedPassword,
+                        role: {
+                            connect: { role_id: 1 },
+                        },
                     },
-                },
-            });
+                });
 
-            const token = app.jwt.sign(
-                {
-                    user_id: user.user_id,
-                    role: user.role_id,
-                },
-                { expiresIn: "7d" },
-            );
+                const token = app.jwt.sign(
+                    {
+                        user_id: user.user_id,
+                        role: user.role_id,
+                    },
+                    { expiresIn: "7d" },
+                );
 
-            return reply.status(201).send({ message: "User registered successfully", user_id: user.user_id, token });
+                return reply
+                    .status(201)
+                    .send({ message: "User registered successfully", user_id: user.user_id, token });
+            } catch (error) {
+                if (error instanceof PrismaClientKnownRequestError) {
+                    if (error.code === "P2002") {
+                        return reply.status(400).send({ message: "Email or username already exists" });
+                    }
+                }
+                console.error("Error registering user:", error);
+                return reply.status(500).send({ message: "Internal server error" });
+            }
         },
     );
 }
