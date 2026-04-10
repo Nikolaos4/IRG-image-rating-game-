f00680bc3b3b9e146e5359929fc65a8046577898
Ksenia Zhuzhleva <zhuzhleva.k.a@edu.mirea.ru>
2026-04-10 11:25:08 +0300

    fix tg account confirmation

--- client/src/api/account.ts
+++ client/src/api/account.ts
@@ -1,6 +1,6 @@
 import { http } from "./http";
 
 export async function postConnectTgRequest() {
-    const response = await http.post<{ code: string }>("/connect-tg");
+    const response = await http.post<{ code: string }>("/account/connect-tg");
     return response.data.code;
 }
